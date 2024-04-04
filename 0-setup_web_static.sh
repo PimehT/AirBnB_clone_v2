256e80367e9e8480c6f0cea14e3382626b9f9a59
PimehT <tareerepimeh@gmail.com>
2024-04-04 20:56:19 +0100

    setup web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,27 +17,13 @@ else
     echo "NGINX already installed"
 fi
 
-# function to check if file exists otherwise create file
-dir_exists() {
-    if [ ! -d "$1" ]; then
-        mkdir -p "$1"
-    else
-        echo "Directory '$1' already exists"
-    fi
-}
-
-# files to check existence otherwise create
-dirs=(
-    "/data/"
-    "/data/web_static/"
-    "/data/web_static/releases/"
-    "/data/web_static/shared/"
-    "/data/web_static/releases/test/"
-)
-
-for dir in "${dirs[@]}"; do
-    dir_exists "$dir"
-done
+test_dir="/data/web_static/releases/test/"
+# check if directory exists otherwise create
+if [ ! -d "$1" ]; then
+    mkdir -p "$test_dir"
+else
+    echo "Directory '$1' already exists"
+fi
 
 # put simple text into index.html for testing
 echo "\
@@ -56,12 +42,11 @@ linked_dir="/data/web_static/releases/test/"
 if [ -d "$sym_link" ]; then
     rm -rf "$sym_link"
 fi
-ln -sf "$linked_dir" "$sym_link"
+ln -s "$linked_dir" "$sym_link"
 echo "symbolic link created"
 
 # give recursive ownership of /data/ to ubuntu and group
-chown -hR ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sudo sed -i '13i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 service nginx restart
